f76e7778ab3b3e103ea0285b527816d9d01c4786
marty_trujillo <marty@ilikeemail.com>
2016-04-26 22:25:03 -0400

    Add A Module

---
+++ examples/000/execute-module/module-example.js
@@ -0,0 +1,10 @@
+var consoleMessage = function(){
+    console.log("Message In The Console");
+};
+var alertMessage = function(){
+    alert("Message In The Alert");  
+};
+
+
+exports consoleMessage = consoleMessage;
+exports alertMessage = alertMessage;
